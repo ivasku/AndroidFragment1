f8b6d0bd1725bea194086c4398289124f1d8263f
Ivasku Adrian <ivaku.adrian@gmail.com>
2019-06-12 00:14:00 +0100

    Added passing data with bundle and with Factory method example

--- app/src/main/java/com/tms/androidfragment01/MainActivity.java
+++ app/src/main/java/com/tms/androidfragment01/MainActivity.java
@@ -1,13 +1,46 @@
 package com.tms.androidfragment01;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Fragment1.OnFragmentInteractionListener, Fragment2.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openFragment(View view) {
+        Fragment1 frag = new Fragment1();
+        Bundle arg = new Bundle();
+        arg.putString("text" , "Passing String to Fragment");
+        arg.putInt("number" , 10);
+        frag.setArguments(arg);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, frag)
+                .commit();
+    }
+
+
+    //to pass data to fragment we use Factory method here
+    // intead of creating the fragment here and passing the bundle
+    // we do that in the Fragment2 class, in the newInstance method
+    public void openFragment2(View view) {
+        Fragment2 frag2 = Fragment2.newInstance("Passing via factory method", 50);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, frag2)
+                .commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //do nothing for now
+    }
+
+
 }
